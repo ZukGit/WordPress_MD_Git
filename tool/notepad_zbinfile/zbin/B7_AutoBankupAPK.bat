dbb866282dd795f58d31f6330b4233137dea5bea
Zukgit-HP-Windows <382581427@qq.com>
2019-05-10 22:02:47 +0800

    x

--- tool/notepad_zbinfile/zbin/B7_AutoBankupAPK.bat
+++ tool/notepad_zbinfile/zbin/B7_AutoBankupAPK.bat
@@ -12,9 +12,10 @@ ECHO 等待您插入手机…
 adb wait-for-device
 adb shell pm list packages -3 -f > ./B7_autoBankupAPP.txt
 @REM 循环安装本目录下的APK文件
+:forbegin
 if exist B7_autoBankupAPP.txt ( 
 @rem 遍历文件多行 
-  for /f %%l in (B7_autoBankupAPP.txt) do ( 
+  for /f %%l in (B7_autoBankupAPP.txt) do (
 set /a num+=1 
 ::set s=%%l
 echo "aaaa"
@@ -35,14 +36,15 @@ if not "!str!"=="" (
 echo !mystr!字符串长度为:%numsize%
 
 
-set ch1==
+set ch1=apk=
 ::注意，这里是区分大小写的！
 set str2=!mystr!
 ::复制字符串，用来截短，而不影响源字符串
 :indexofnext
+set prestr=!str2:~0,4! 
 if not "!str2!"=="" (
 set /a index+=1
-if "!str2:~0,1!"=="%ch1%" goto last
+if "!prestr!"=="%ch1% " goto last
 ::比较首字符是否为要求的字符，如果是则跳出循环
 set "str2=!str2:~1!"
 goto indexofnext
@@ -54,26 +56,35 @@ echo 字符'%ch1%'在字符串"!mystr!"中的首次出现位置为%index%
 
 
 ::  长度82 , = 的位置是56
-set /a pos = %index% - 9 
+set /a pos = %index% -6
 set apkpath=!mystr!
-set apkpath=!mystr:~8,%index%!
+set apkpath=!mystr:~8,%pos%!
 set packagename=!mystr!
-set packagename=!mystr:~%index%,%numsize%!
-echo 安装路径为'!apkpath!'包名为"!packagename!"
+set /a pos = %index% + 3
+set packagename=!mystr:~%pos%,%numsize%!
+::echo 安装路径为'!apkpath!'包名为"!packagename!"
 	   
 @rem 	echo !s%! >> B7_test.txt
 ::       ECHO 卸载包:  !s%!
 ::       call adb uninstall !s%!
+call adb pull !apkpath!  ./"!packagename!".apk
+
 set /a index = 0
 set /a pos = 0
 set /a numsize = 0
-  )
-ECHO 手机三方应用已完全卸载！   
+
+more +1 B7_autoBankupAPP.txt > a.tmp
+del B7_autoBankupAPP.txt
+ren a.tmp B7_autoBankupAPP.txt
+
+goto forbegin
+)
+ECHO 手机中的三方APK以完全拉取完成   
 ) else (
-ECHO B7_uninstallpacket.txt不存在！ 
+ECHO B7_autoBankupAPP.txt不存在！ 
 )
 
-ECHO 卸载三方APK好了；换一台手机卸载吧！！！
+ECHO 换一台手机进行拉取APK吧！！！
 PAUSE
 GOTO LOOP
 
