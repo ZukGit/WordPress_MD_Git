db36041da8a7075f242acc57b34340ae499a2788
Zukgit-HP-Windows <382581427@qq.com>
2019-05-11 08:33:52 +0800

    x

--- tool/notepad_zbinfile/zbin/B7_zTakeScreenShot.bat
+++ tool/notepad_zbinfile/zbin/B7_zTakeScreenShot.bat
@@ -13,16 +13,20 @@ adb wait-for-device
 
 @rem adb shell pm list packages -3 > ./B7_uninstallpacket.txt
 
-set CURRENT_DATE_TIME_STAMP=%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+set hh=%time:~0,2%
+if /i %hh% LSS 10 (set hh=0%time:~1,1%)
+
+set CURRENT_DATE_TIME_STAMP=%date:~0,4%%date:~5,2%%date:~8,2%_%hh%%time:~3,2%%time:~6,2%
 
 
 adb shell screencap -p /sdcard/Pictures/B7_zscreenshot_%CURRENT_DATE_TIME_STAMP%.png
 
-
-
+ECHO ½ØÍ¼Ãû³Æ/sdcard/Pictures/B7_zscreenshot_%CURRENT_DATE_TIME_STAMP%.png 
+@rem ½ØÍ¼Ãû³Æ/sdcard/Pictures/B7_zscreenshot_20190511_ 80010.png
 
 adb pull /sdcard/Pictures/B7_zscreenshot_%CURRENT_DATE_TIME_STAMP%.png  ./B7_zscreenshot_%CURRENT_DATE_TIME_STAMP%.png
 
+ECHO   ½áÊø£¡
 PAUSE
 GOTO LOOP
 @ECHO on
