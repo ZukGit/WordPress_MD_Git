9ae782dd06772601022c56ee6eb1cd84ea1e47c3
Zukgit-windos10-Mac <382581427@qq.com>
2019-07-29 14:40:54 +0800

    2019年07月29日-14时40分53秒______Zukgit-的提交

--- tool/notepad_zbinfile/zbin/win_zbin/zcommit.bat
+++ tool/notepad_zbinfile/zbin/win_zbin/zcommit.bat
@@ -1,7 +1,9 @@
 @ECHO off
 Setlocal enabledelayedexpansion
-git add ./
+set hh=%time:~0,2%
+if /i %hh% LSS 10 (set hh=0%time:~1,1%)
 set CURRENT_DATE_TIME_STAMP=%date:~0,4%年%date:~5,2%月%date:~8,2%日-%hh%时%time:~3,2%分%time:~6,2%秒
+git add ./
 git commit -m %CURRENT_DATE_TIME_STAMP%______Zukgit-的提交
 git push
 @pause
